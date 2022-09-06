9ce5c925722f619a1a9b8de75e3c7f2aea5f9197
Dickson <kibedickson3@gmail.com>
2022-09-06 10:30:29 +0300

    installed & configured tailwindcss, prettier, headlessui, heroicons, vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+    modules: ["nuxt-storm", "@nuxtjs/tailwindcss"],
+    css: ["@/assets/css/main.css"],
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                },
+            },
+        },
+    },
+});
